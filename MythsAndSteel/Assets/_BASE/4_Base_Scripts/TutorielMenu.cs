ccf9767a3b73c30bdd9053e1e9bdbd00f228ed4c
BGauthier59 <80094955+BGauthier59@users.noreply.github.com>
2021-07-22 23:34:36 +0200

    Update TutorielMenu.cs

--- MythsAndSteel/Assets/_BASE/4_Base_Scripts/TutorielMenu.cs
+++ MythsAndSteel/Assets/_BASE/4_Base_Scripts/TutorielMenu.cs
@@ -54,21 +54,21 @@ public class TutorielMenu : MonoBehaviour
 
         if (TutorielIndex < 8)
         {
-            UnitesIcon.GetComponent<Image>().color = new Color(255, 190, 190, 255);
+            UnitesIcon.GetComponent<Image>().color = Color.yellow;
             PhasesDeJeuIcon.GetComponent<Image>().color = Color.white;
             MecaniquesIcon.GetComponent<Image>().color = Color.white;
         }
         if (TutorielIndex >= 8 && TutorielIndex < 14)
         {
             UnitesIcon.GetComponent<Image>().color = Color.white;
-            PhasesDeJeuIcon.GetComponent<Image>().color = new Color(255, 190, 190, 255);
+            PhasesDeJeuIcon.GetComponent<Image>().color = Color.yellow;
             MecaniquesIcon.GetComponent<Image>().color = Color.white;
         }
         if (TutorielIndex >= 14)
         {
             UnitesIcon.GetComponent<Image>().color = Color.white;
             PhasesDeJeuIcon.GetComponent<Image>().color = Color.white;
-            MecaniquesIcon.GetComponent<Image>().color = new Color(255, 190, 190, 255);
+            MecaniquesIcon.GetComponent<Image>().color = Color.yellow;
         }
 
         switch (TutorielIndex)
