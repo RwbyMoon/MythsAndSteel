4dde29d41b3d87921dd1cfcb70d210aa52d6a78d
Benjamin Andula <andula.benjamin@bbox.fr>
2021-07-28 02:49:34 +0200

    Fix UI bouton Phase Suivante + Pressed visual

--- MythsAndSteel/Assets/MetaGame/Scripts/HoverChangeColor.cs
+++ MythsAndSteel/Assets/MetaGame/Scripts/HoverChangeColor.cs
@@ -6,19 +6,39 @@ using UnityEngine.UI;
 
 public class HoverChangeColor : MonoBehaviour
 {
-    [SerializeField]private Color startColor;
-    [SerializeField] private Color mouseOverColor;
+    [SerializeField]private Color startColor = new Color(255, 255, 255, 255);
+    [SerializeField] private Color mouseOverColor = new Color(221, 221, 221, 255);
+    [SerializeField] private Color pressedColor = new Color(160, 160, 160, 255);
     [SerializeField] private GameObject changedTarget;
     bool mouseOver = false;
+    bool isClicked = false;
 
     public void OnMouseEnter()
     {
-        mouseOver = true;
-        changedTarget.GetComponent<Image>().color = mouseOverColor;
+        if (isClicked == false)
+        {
+            mouseOver = true;
+            changedTarget.GetComponent<Image>().color = mouseOverColor;
+        }
     }
     public void OnMouseExit()
     {
-        mouseOver = false;
+        if (isClicked == false)
+        {
+            mouseOver = false;
+            changedTarget.GetComponent<Image>().color = startColor;
+        }
+        
+    }
+    public void OnMouseDown()
+    {
+        isClicked = true;
+        changedTarget.GetComponent<Image>().color = pressedColor;
+    }
+
+    public void OnMouseUp()
+    {
+        isClicked = false;
         changedTarget.GetComponent<Image>().color = startColor;
     }
 }
