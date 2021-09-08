367f54ad0e506e503a60e4e1fe33b493ce9d079f
Bouloche <vmabille02@gmail.com>
2021-09-08 10:12:15 +0200

    Accélération
    +Accélération est codé et fonctionnel, !!Manque les fumigènes pour compléter le code!!

---
+++ MythsAndSteel/Assets/Groupes/Gr1_Rethel/Scripts/Acceleration.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration : MonoBehaviour
+{
+    bool BoostLaunch;
+    bool BoostAct;
+
+    void Update()
+    {
+        if(GetComponent<UnitScript>().NewTurnHasStart && !BoostLaunch)
+        {
+            BoostLaunch = true;
+            foreach (int i in PlayerStatic.GetNeighbourDiag(GetComponent<UnitScript>().ActualTiledId, TilesManager.Instance.TileList[gameObject.GetComponent<UnitScript>().ActualTiledId].GetComponent<TileScript>().Line, false))
+            {
+                if((TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Feu) || TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Brasier)) && !BoostAct)
+                {
+                    BoostAct = true;
+                    GetComponent<UnitScript>().MoveSpeedBonus++;
+                }
+            }
+        }
+        if(GameManager.Instance.IsNextPhaseDone && BoostLaunch)
+        {
+            BoostLaunch = false;
+            BoostAct = false;
+        }
+    }
+}
