066d4ba8aba027ecc6f472ed8cd3dd1579f0566f
Bouloche <vmabille02@gmail.com>
2021-09-27 15:42:12 +0200

    Oui

--- MythsAndSteel/Assets/Groupes/Gr1_Rethel/Scripts/Acceleration.cs
+++ MythsAndSteel/Assets/Groupes/Gr1_Rethel/Scripts/Acceleration.cs
@@ -14,7 +14,7 @@ public class Acceleration : MonoBehaviour
             BoostLaunch = true;
             foreach (int i in PlayerStatic.GetNeighbourDiag(GetComponent<UnitScript>().ActualTiledId, TilesManager.Instance.TileList[gameObject.GetComponent<UnitScript>().ActualTiledId].GetComponent<TileScript>().Line, false))
             {
-                if((TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Feu) || TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Brasier)) && !BoostAct)
+                if((TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Feu) || TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Brasier) || TilesManager.Instance.TileList[i].GetComponent<TileScript>().TerrainEffectList.Contains(MYthsAndSteel_Enum.TerrainType.Fumigène)) && !BoostAct)
                 {
                     BoostAct = true;
                     GetComponent<UnitScript>().MoveSpeedBonus++;
